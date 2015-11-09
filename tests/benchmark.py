49aca1445f1db5ee5bb7ed0ca0ceacd3b9757891
Ultrabug <ultrabug@gentoo.org>
2015-11-09 11:55:29 +0100

    allow benchmark to run without ketama

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 """This is not part of the test suite.
 """
-import ketama
+try:
+    import ketama
+except Exception:
+    ketama = None
 
 from time import time
 from tempfile import NamedTemporaryFile
@@ -11,17 +14,18 @@ num = 1000000
 print('running {} key generation comparison'.format(num))
 
 # ketama C binding
-with NamedTemporaryFile(prefix='benchmark_') as ketama_config_file:
-    ketama_config_file.write("127.0.0.1:11211\t600\n")
-    ketama_config_file.write("127.0.0.1:11212\t400\n")
-    ketama_config_file.flush()
+if ketama:
+    with NamedTemporaryFile(prefix='benchmark_') as ketama_config_file:
+        ketama_config_file.write("127.0.0.1:11211\t600\n")
+        ketama_config_file.write("127.0.0.1:11212\t400\n")
+        ketama_config_file.flush()
 
-    kt = ketama.Continuum(ketama_config_file.name)
-    pt = time()
-    for i in range(num):
-        key = 'myval-{}'.format(i)
-        kt.get_server(key)
-    print('ketama took {} ms'.format(time() - pt))
+        kt = ketama.Continuum(ketama_config_file.name)
+        pt = time()
+        for i in range(num):
+            key = 'myval-{}'.format(i)
+            kt.get_server(key)
+        print('ketama took {} ms'.format(time() - pt))
 
 # pure python implementation
 ring = HashRing(
