59c2743db7a0dafa6e0fe0173e04451ffdf9951f
Ultrabug <ultrabug@gentoo.org>
2017-07-10 13:07:31 +0200

    version 0.6

--- setup.py
+++ setup.py
@@ -67,5 +67,5 @@ setup(
     platforms='any',
     tests_require=['python-memcached', 'pytest'],
     url='https://github.com/ultrabug/uhashring',
-    version='0.5',
+    version='0.6',
     zip_safe=True)
